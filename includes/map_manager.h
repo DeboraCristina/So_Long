8cd67cf16e59941202ec94c7576719f2ea73d3b1
DeboraCristina <deboracristinaproficional1@gmail.com>
2022-07-25 20:09:30 -0300

    move images

---
+++ includes/map_manager.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   map_manager.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: desilva <dede-2231@hotmail.com>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/25 13:30:30 by desilva           #+#    #+#             */
+/*   Updated: 2022/07/25 13:30:32 by desilva          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MAP_MANAGER_H
+# define MAP_MANAGER_H
+
+// map utils
+int		row_counter(char *name);
+
+// map checker
+int		valid_map_elements(char *name);
+int		minimum_elements(char *name);
+int		check_map_name(char *name);
+int		check_char(char *set, char *str);
+int		check_linelen(int *default_len, int index, char *row);
+int		valid_elements(int index, int last, char *row);
+void	gen_minimuns(int *minimum, char *name);
+
+// map configing
+void	gen_maplst(char *name, t_map *p_map);
+
+#endif
